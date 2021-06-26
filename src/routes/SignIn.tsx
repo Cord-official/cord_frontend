8628637454eeadf9e910a15db1c1acff1cf6ce51
onion8513 <jsi090224@naver.com>
2021-06-27 02:44:38 +0900

    feat: SignIn

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -1,12 +1,66 @@
 import React, { useState } from 'react'
-
+import { client, gql } from '../client'
 const SignIn = () => {
-  const [Username_Email, SetUsername_Email] = useState('')
+  const [Name_Email, SetName_Email] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget
+
+    switch (name) {
+      case 'Name_Email':
+        SetName_Email(value)
+        break
+      case 'password':
+        setPassword(value)
+        break
+      default:
+        break
+    }
+  }
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (Name_Email && password) {
+      const emailRule =
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+
+      const { data } = await client.query({
+        query: gql`
+          query {
+            signIn(${
+              emailRule.test(Name_Email) ? `email: "${Name_Email}" ` : `name: "${Name_Email}"`
+            }, password: "${password}") {
+              ok
+              token
+              error
+            }
+          }
+        `
+      })
+      console.log(data)
+      setError('')
+    } else {
+      setError('필수항목은 작성해야지!!')
+    }
+  }
 
   return (
     <>
-      <div>a</div>
+      <div>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="Name_Email">
+            <span>name/email</span>
+            <input type="text" name="Name_Email" onChange={onChange} />
+          </label>
+          <label htmlFor="password">
+            <span>password</span>
+            <input type="password" name="password" onChange={onChange} />
+          </label>
+          <input type="submit" />
+        </form>
+      </div>
+      <div>{error}</div>
     </>
   )
 }
